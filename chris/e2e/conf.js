81aba91b77d61303b060c2f72d2dc5ec1a953a01
tom_gr_chen <tom_gr_chen@trendmicro.com>
2020-02-19 14:29:09 +0800

    add e2e test

---
+++ chris/e2e/conf.js
@@ -0,0 +1,30 @@
+exports.config = {
+  //自動幫忙啟動 Selenium Server 
+  directConnect: true,
+
+
+  // 設定啟用的 webdriver
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+
+  // 設定使用的 testing framework
+  framework: 'jasmine',
+
+
+  // 指定要跑的 test file
+  specs: ['example_spec.js'],
+
+
+
+  // 啟用 ES7 async, await 非同步語法糖
+  // 代表 protractor 不會再幫你 handle promise
+  // 需自行處理非同步方法
+  SELENIUM_PROMISE_MANAGER: true,
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
---
+++ chris/e2e/example_spec.js
@@ -0,0 +1,24 @@
+describe('angularjs homepage', function() {
+    beforeAll(async () => {
+        browser.waitForAngularEnabled(false);
+    });
+
+    it('go to budget page', function() {
+      browser.get('http://localhost:8000/budget/');
+    });
+
+    it('input budget', function() {
+        element(by.id('date')).sendKeys('202002');
+        element(by.id('budget')).sendKeys('5000');
+        element(by.id('submit')).click();
+    });
+
+    it('success', function() {
+        const result = element(by.css('body')).getText();
+        expect(result).toBe('update budget successful.');
+        browser.sleep(2000);
+    });
+  });
+
+
+  
